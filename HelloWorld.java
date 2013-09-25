f6b876b05b6176969ef88c35524def5a8f5da80f
Rojonh <rojonh23@gmail.com>
2013-09-25 13:17:55 +0800

    bura

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld{
   public static void main(String args[]){
     System.out.println("HelloWorld");
-    System.out.println("New World");
-    System.out.println("Big World");
-     System.out.println("Big World + 2e32");
   }
 }
